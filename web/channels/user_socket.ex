8ea20449c77db4bcfb6822de7c15c0000eb80efd
Nick Sergeant <nick@nicksergeant.com>
2016-04-10 21:21:42 -0400

    Wabsockets?

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -3,8 +3,9 @@ defmodule RatedBeer.UserSocket do
 
   channel "ratedbeer:*", RatedBeer.SearchChannel
 
-  transport :websocket, Phoenix.Transports.WebSocket
   transport :longpoll, Phoenix.Transports.LongPoll
+  transport :websocket, Phoenix.Transports.WebSocket,
+    origins: ["http://ratedbeer.com", "https://ratedbeer.com"]
 
   def connect(_params, socket) do
     {:ok, socket}
